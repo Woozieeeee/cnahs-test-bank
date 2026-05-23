abb653b79503c480468333bc887e734b1719aa19
Woozieeeee <mechtitan23@gmail.com>
2026-05-23 09:10:16 +0800

    .

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,36 +1,5 @@
-import { defineConfig, globalIgnores } from "eslint/config";
+import type { NextConfig } from "next";
 
-import nextVitals from "eslint-config-next/core-web-vitals";
+const nextConfig: NextConfig = {};
 
-import nextTs from "eslint-config-next/typescript";
-
-import prettierPlugin from "eslint-plugin-prettier";
-
-import prettierConfig from "eslint-config-prettier";
-
-const eslintConfig = defineConfig([
-  ...nextVitals,
-
-  ...nextTs,
-
-  {
-    plugins: {
-      prettier: prettierPlugin,
-    },
-
-    rules: {
-      "prettier/prettier": "warn",
-    },
-  },
-
-  prettierConfig,
-
-  globalIgnores([
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
-
-export default eslintConfig;
+export default nextConfig;
